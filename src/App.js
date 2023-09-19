692b4f7b00d2b64e3f501179f618d3fafd0b1d08
pinczesbalint977 <pinczesbalint1997@gmail.com>
2023-09-19 17:22:40 +0200

    Létrehoztam az oldalakat,aloldalakat és annak útjait

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Names from './Pages/Names';
+import Emails from './Pages/Emails';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Kezdőlap</Link></li>
+            <li><Link to="/Names">Nevek</Link></li>
+            <li><Link to="/Emails">Email</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/Names' element={<Names />}/>
+          <Route path='/Emails' element={<Emails />}/>
+        </Routes>
+
+
+      <footer>Copyright &copy; 2023</footer>
+    </BrowserRouter>
   );
 }
 
