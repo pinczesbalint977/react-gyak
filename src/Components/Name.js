fd6a229c82d1a9895896b59d766dc388157c3249
pinczesbalint977 <pinczesbalint1997@gmail.com>
2023-09-19 17:33:04 +0200

    Nevek oldal apijának lekérése és megjelenítése

---
+++ src/Components/Name.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Name = ({name}) => {
+  return (
+    <div>
+        <h2>Név: {name}</h2>
+    </div>
+  )
+}
+
+export default Name
--- src/Pages/Names.js
+++ src/Pages/Names.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Name from "../Components/Name"
 
 const Names = () => {
+
+    const [names,setNames] = useState([]);
+
+    useEffect(() => {
+
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(res => setNames(res.data))
+        .catch(err =>console.log(err))
+
+    },[])
+
   return (
-    <div>Names</div>
+    <div>
+
+        {
+            names.map((user,idx) => {
+
+                return <Name key={idx} name={user.name} />
+            })
+        }
+    </div>
   )
 }
 
